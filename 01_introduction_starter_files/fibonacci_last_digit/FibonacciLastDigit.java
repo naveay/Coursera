b6a072b2f6c53315ec79087b95276edbca426484
naveay <jyxin@uci.edu>
2017-11-02 11:39:21 -0700

    modified:   01_introduction_starter_files/fibonacci_last_digit/FibonacciLastDigit.java

--- 01_introduction_starter_files/fibonacci_last_digit/FibonacciLastDigit.java
+++ 01_introduction_starter_files/fibonacci_last_digit/FibonacciLastDigit.java
@@ -9,14 +9,15 @@ public class FibonacciLastDigit {
         int current  = 1;
 
         for (int i = 0; i < n - 1; ++i) {
-            int tmp_previous = previous;
-            previous = current;
-            current = tmp_previous + current;
+            current = previous + current;
+            previous = current-previous;
+            current=current%10;
+            previous=previous%10;
         }
 
-        return current % 10;
+        return current;
     }
-    
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
@@ -24,4 +25,3 @@ public class FibonacciLastDigit {
         System.out.println(c);
     }
 }
-
